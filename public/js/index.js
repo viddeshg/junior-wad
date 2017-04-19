2d73d32644c8c90d715dc86a6ace0e686e5a6628
Viddesh Gharpure <viddesh.gharpure@gmail.com>
2017-04-20 00:46:27 +0530

    removed google icons to font awesome

--- public/js/index.js
+++ public/js/index.js
@@ -11,5 +11,14 @@ jQuery(document).ready(function ($) {
     $('.modal').modal();
     $('.scrollspy').scrollSpy();
     $(".button-collapse").sideNav();
+
+      $('.button-collapse').sideNav({
+      menuWidth: 250, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+    
 });
 
