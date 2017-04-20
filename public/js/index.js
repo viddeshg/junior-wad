b9bd9e8a71c7c00f2a3c779b4ca51edb2ac627fe
Viddesh Gharpure <viddesh.gharpure@gmail.com>
2017-04-20 10:16:01 +0530

    Added extempore form, changed navbar a bit

--- public/js/index.js
+++ public/js/index.js
@@ -14,11 +14,13 @@ jQuery(document).ready(function ($) {
 
       $('.button-collapse').sideNav({
       menuWidth: 250, // Default is 300
-      edge: 'left', // Choose the horizontal origin
+      edge: 'right', // Choose the horizontal origin
       closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
 
+  var disclaimer = $('.freebirdFormviewerViewFooterDisclaimer');
+  disclaimer.remove();
 });
 
